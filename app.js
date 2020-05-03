d01f0ad7d6b5263c6b87ba5e2a945cba3283d710
Alvaro Centeno <alvaroc0689@gmail.com>
2020-05-02 22:24:05 -0700

    added function that detects the current time and changes the classes of each

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ function currentDate() {
     $("#currentDay").text(momentDate);
 };
 currentDate();
+
+var date = new Date();
+console.log(date);
+var currentHour = date.getHours();
+
+
+$(".userInput").each(function () {
+
+    if ($(this).attr("time") == currentHour) {
+        $(this).addClass("present");
+    };
+
+    if ($(this).attr("time") > currentHour) {
+        $(this).addClass("future");
+    };
+
+    if ($(this).attr("time") < currentHour) {
+        $(this).addClass("past");
+    };
+
+});
+
