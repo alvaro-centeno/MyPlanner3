e0d1216171325f65283e9990a2b1480f26799713
Alvaro Centeno <alvaroc0689@gmail.com>
2020-05-02 23:22:43 -0700

    added local storage to store data and show when page is refreshed

--- app.js
+++ app.js
@@ -1,27 +1,50 @@
-function currentDate() {
-    var momentDate = moment().format('LLLL');
-    $("#currentDay").text(momentDate);
-};
-currentDate();
+$(document).ready(function () {
 
-var date = new Date();
-console.log(date);
-var currentHour = date.getHours();
-
-
-$(".userInput").each(function () {
-
-    if ($(this).attr("time") == currentHour) {
-        $(this).addClass("present");
+    function currentDate() {
+        var momentDate = moment().format('LLLL');
+        $("#currentDay").text(momentDate);
     };
+    currentDate();
 
-    if ($(this).attr("time") > currentHour) {
-        $(this).addClass("future");
-    };
+    var date = new Date();
 
-    if ($(this).attr("time") < currentHour) {
-        $(this).addClass("past");
-    };
+    var currentHour = date.getHours();
 
+    $(".userInput").each(function () {
+
+        if ($(this).attr("time") == currentHour) {
+            $(this).addClass("present");
+        };
+
+        if ($(this).attr("time") > currentHour) {
+            $(this).addClass("future");
+        };
+
+        if ($(this).attr("time") < currentHour) {
+            $(this).addClass("past");
+        };
+
+    });
+
+    var planLogs = JSON.parse(localStorage.getItem("planLogs"));
+    // console.log(planLogs);
+
+    var areaText = $("textArea[name='text'");
+    // console.log(areaText);
+
+    $(areaText).each(function () {
+        $(this).val(planLogs[$(this).attr("time")]);
+    });
+
+    $("button").on("click", function () {
+
+        $(areaText).each(function () {
+            (planLogs)[$(this).attr("time")] = $(this).val();
+
+            localStorage.setItem("planLogs", JSON.stringify(planLogs));
+
+        });
+
+
+    });
 });
-
